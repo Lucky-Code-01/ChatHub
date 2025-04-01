d5716f8c7ef4b83ad4509777767c8f75cf15b023
lucky-code-01 <luckybaisoya003@gmail.com>
2025-04-01 11:43:59 +0530

    uploading files

---
+++ Index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const {Server} = require('socket.io');
+const app = express();
+const users = {};
+
+// convert express server to http
+const server = http.createServer(app);
+
+// now integrate or join the socket to http server
+const io = new Server(server);
+
+// middle warr
+// Static files serve karna
+app.use(express.static(path.join(__dirname, 'public', 'ChatHub')));
+
+// established the connection
+io.on('connection',(socket)=>{
+
+    // to join new user
+    socket.on('new-user-add',(name)=>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    })
+
+    // receive msg
+    socket.on('send',(message)=>{
+        socket.broadcast.emit('recive',{message:message,name:users[socket.id]})
+    })
+
+    // disconnet 
+    socket.on('disconnect',(message)=>{
+        socket.broadcast.emit('left',users[socket.id]);
+    })
+})
+
+
+server.listen(8080,()=>{
+    console.log("Server Running On Port :- http://localhost:8080");
+})
---
+++ public/ChatHub/Chat.js
@@ -0,0 +1,48 @@
+const socket = io();
+const userName = prompt("Enter your name :- ");
+const msgbox = document.querySelector(".chat-box");
+const input = document.querySelector(".msginput");
+const form = document.querySelector(".send-form"); 
+const msgSound = new Audio('./ImageAudio/Msg.mp3');
+
+// notification function
+function showNotification(message,position){
+    const msg = document.createElement("div");
+    msg.classList.add(position);
+    msg.textContent = message;
+    msgbox.append(msg);
+    if(position == 'left'){
+        msgSound.play();
+    }
+}
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let msgtext = input.value;
+    showNotification(msgtext,"right");
+    socket.emit('send',msgtext);
+    input.value = '';
+    msgbox.scrollTo({ top:msgbox.scrollHeight,behavior: 'smooth' });
+    
+})
+
+
+// event handle 
+socket.emit('new-user-add',userName);
+socket.on('user-joined',(name)=>{
+    showNotification(`${name}: joined the chat`,"left");
+    console.log(name);
+})
+
+socket.on('recive',(message)=>{
+    showNotification(`${message.name}: ${message.message}`,"left");
+})
+
+// disconnect or left the chat
+socket.on('left',(data)=>{
+    showNotification(`${data}: Left the chat`,"left");
+})
+
+
+// function to send msg
